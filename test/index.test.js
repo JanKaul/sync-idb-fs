8050440d42294f3fa5266ef8d102dbbbb4709bd4
JanKaul <jankaul@mailbox.org>
2022-11-23 13:53:55 +0100

    update tests

--- test/index.test.js
+++ test/index.test.js
@@ -7,15 +7,39 @@ it('test set get', async () => {
 
     await timeout(100);
 
-    let data = new Uint8Array([1,2,3,4,5,6,7]);
-
-    fs.writeFileSync("/my_file", data);
+    fs.mkdirSync("/src")
 
     await timeout(100);
 
-    let result = fs.readFileSync("/my_file")
+    let dir = fs.statSync("/src").type
+
+    expect(dir).to.equal('dir');
+
+    let data = new Uint8Array([1,2,3,4,5,6,7]);
+
+    fs.writeFileSync("/src/index.js", data);
+
+    await timeout(100);
+
+    let exists = fs.existsSync("/src/index.js")
+
+    expect(exists).to.equal(true)
+
+    let file = fs.statSync("/src/index.js").type
+
+    expect(file).to.equal('file')
+
+    let result = fs.readFileSync("/src/index.js")
 
     expect(result).to.eq(data)
+
+    fs.unlinkSync("/src/index.js")
+
+    await timeout(100);
+
+    let not_exists = fs.existsSync("/src/index.js")
+
+    expect(not_exists).to.equal(false)
 });
 
 it('test root', async () => {
